1cd338a48e9f3930d60f167551794fd87029d818
AndresSern <andrecamposern@gmail.com>
2021-04-05 02:19:37 +0000

    Putting a letter 'w'

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in /tmp
-file{'tmp/holberton':
+file {'tmp/holberton':
      path    => '/tmp/holberton',
      mode    => '0744',
-     owner   => 'ww-data',
+     owner   => 'www-data',
      group   => 'www-data',
      content => 'I love Puppet'
      }
