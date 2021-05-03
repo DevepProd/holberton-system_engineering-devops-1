2259ec2c6ec02d24f0a610202e77659b6fa31f0e
AndresSern <andrecamposer@gmail.com>
2021-05-03 14:15:41 -0500

    Fixing style and removing lines, and deleting the file 2.json

--- 0x15-api/2-export_to_json.py
+++ 0x15-api/2-export_to_json.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-# Python Script that, using this REST API, for a given employee ID, returns
-# information about his/her TODO list progress
+""" Python script to export data in the JSON FORMAT """
 import requests
 from sys import argv
 import json
 
 if __name__ == "__main__":
     user_id = argv[1]
-
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get((url + "users/%s") % user_id).json()
     tasks = requests.get((url + "todos/?userId=%s") % user_id).json()
-
     myjsonDict = {}
     myjsonDict[user_id] = []
 
     for task in tasks:
         myjsonDict[user_id].append({"task": task["title"],
-                                   "completed": task["completed"],
-                                   "username": user["username"]})
+                                    "completed": task["completed"],
+                                    "username": user["username"]})
 
     with open("{}.json".format(user_id), "w") as jsonfile:
         jsonfile.write((json.dumps(myjsonDict)))
