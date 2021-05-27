347ab079f12febeb1ea498e36784b46f8699e464
AndresSern <andrecamposer@gmail.com>
2021-05-26 23:15:32 -0500

    Adding the file 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# Sky is the limit, let's bring that limit higher mandatory
+# Puppet to fix limit number request to unlimit number request with nginx
+exec {'Unlimit Request':
+	command => "sed -i 's/ULIMIT=/# ULIMIT=/g' /etc/default/nginx",
+	path    => ["/bin", "/sbin", "usr/bin", "/usr/sbin"],
+}
+
+exec {'restart nginx':
+	command => "sudo service nginx restart",
+	path    => ["/bin", "/sbin", "usr/bin", "/usr/sbin"],
+} ~>
+
+service {'nginx':
+	ensure	=> running,
+}
