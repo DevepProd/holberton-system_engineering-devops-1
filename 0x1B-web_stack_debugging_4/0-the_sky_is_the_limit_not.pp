6901a4818b34b8d168dc5e6cbca5b52be3edc566
AndresSern <andrecamposer@gmail.com>
2021-05-27 00:18:13 -0500

    FIxing style with puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,14 +2,14 @@
 # Puppet to fix limit number request to unlimit number request with nginx
 exec {'Unlimit Request':
     command => "sed -i 's/ULIMIT=/# ULIMIT=/g' /etc/default/nginx",
-    path    => ["/bin", "/sbin", "usr/bin", "/usr/sbin"],
+    path    => ['/bin', '/sbin', 'usr/bin', '/usr/sbin'],
 }
 
 exec {'restart nginx':
-    command => "sudo service nginx restart",
-    path    => ["/bin", "/sbin", "usr/bin", "/usr/sbin"],
+    command => 'sudo service nginx restart',
+    path    => ['/bin', '/sbin', 'usr/bin', '/usr/sbin'],
 }
 
 service {'nginx':
-    ensure	=> running,
+    ensure  => running,
 }
